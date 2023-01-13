9defb8e218fa8954dc218fde3a4082f0a71b03b0
Austin Bowen <austin.bowen.314@gmail.com>
2023-01-13 03:17:20 -0500

    Send drive commands to web server via WebSockets

--- src/python/website/main.py
+++ src/python/website/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -8,12 +10,27 @@ def index():
     return render_template('index.html')
 
 
+@socketio.on('disconnect')
+def test_disconnect():
+    handle_motor_controller_stop()
+
+
+@socketio.on('motorController.drive')
+def handle_motor_controller_drive(args):
+    print(f'drive: {args}')
+
+
+@socketio.on('motorController.stop')
+def handle_motor_controller_stop():
+    print('stop')
+
+
 def main():
-    app.run(
+    socketio.run(
+        app,
         host='0.0.0.0',
         port=8080,
         debug=True,
-        threaded=True,
         use_reloader=False,
     )
 
