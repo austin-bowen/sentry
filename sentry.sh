058648c3486b33792245ca5f99d7047843ae9c94
Austin Bowen <austin.bowen.314@gmail.com>
2023-01-15 17:57:24 -0500

    Fix service startup

--- sentry.sh
+++ sentry.sh
@@ -8,10 +8,9 @@ source ./bin/activate
 PYTHONPATH=$ROOT/src/python:$ROOT/lib/python/serial-packets/src/python
 export PYTHONPATH
 
-cd "$ROOT/src/python/stream-video/" && {
-  python main.py > "$LOG/stream-video.log" 2>&1 &
-}
+cd "$ROOT/src/python/stream-video/" || exit 1
+python main.py > "$LOG/stream-video.log" 2>&1 &
 
-cd "$ROOT/src/python/website/" && {
-  python main.py > "$LOG/website.log" 2>&1 &
-}
+
+cd "$ROOT/src/python/website/" || exit 2
+python main.py > "$LOG/website.log" 2>&1 &
